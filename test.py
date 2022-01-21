f6e73dcabd031e81502961f5edf742a6f67a4f02
Ji ZHANG <hope.zh@gmail.com>
2022-01-21 17:38:40 +0800

    add todo tree marks

--- test.py
+++ test.py
@@ -7,3 +7,20 @@ y = torch.ones(2, 3)
 print(y, '\n')
 
 print('cuda available: ', torch.cuda.is_available(), '\n')
+
+# [#] Level 1 
+# [+] Level 2
+# [-] Level 3
+# [.] Level 4
+
+# [T] Test
+# [x] Checked
+
+# BUG
+# COMMENT
+# CLOSED
+# DONE
+# FIXME
+# HACK
+# NOTE
+# TODO
